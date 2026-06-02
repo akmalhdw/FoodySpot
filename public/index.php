9eaafdb3a616d2d601aa81f92b57113e479b5716
akmalhdw <hidayatakmal799@gmail.com>
2026-06-02 22:53:18 +0700

    Resolve README conflict

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Tentukan jalur ke maintenance mode jika ada
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Registrasi Autoloader Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Jalankan Aplikasi Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$handle = $app->make(Request::class);
+
+$response = $handle->handle(
+    $request = Request::capture()
+)->send();
+
+$handle->terminate($request, $response);
